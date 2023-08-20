c0d1e41597099f2312670556f618ec4d4d1198db
Roman Shahtarov <abaltis@yandex.ru>
2023-08-20 14:56:58 +0300

    разбиение на модули 2

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,51 @@
 //Функции для работы с карточками
+
+import {
+	increaseImage
+} from "./modal.js";
+
+import {
+	addLike,
+	handleDelButton
+} from "./../index.js";
+
+// Увеличение изображения
+const popupImage = document.querySelector('.popup_type_image');
+const imageZoom = document.querySelector('.popup__image');
+const imageTitle = document.querySelector('.popup__image-title');
+
+const elementTemplate = document.querySelector('#element').content;
+
+export const elements = document.querySelector('.elements');
+
+// Добавление карточки на страницу
+// export function addСardToPage(elementName, elementLink) {
+// 	const card = createCard(elementName, elementLink);
+// 	elements.prepend(card);
+// }
+
+// Создание карточки
+export function createCard(elementName, elementLink) {
+	const cardElement = elementTemplate.querySelector('.element').cloneNode(true);
+	const elementImage = cardElement.querySelector('.element__image');
+	elementImage.src = elementLink;
+	elementImage.alt = elementName;
+
+	const elementHeader = cardElement.querySelector('.element__header');
+	elementHeader.textContent = elementName;
+
+	// Добавление лайка
+	const like = cardElement.querySelector('.element__like');
+	addLike(like);
+
+	// Удаление карочки
+	const deleteButton = cardElement.querySelector('.element__delete');
+	handleDelButton(deleteButton, cardElement);
+
+	// Увеличение изображения
+	elementImage.addEventListener('click', (e) => {
+		increaseImage(popupImage, imageZoom, imageTitle, elementLink, elementName);
+	});
+
+	return cardElement;
+}
--- src/index.js
+++ src/index.js
@@ -7,23 +7,16 @@ import {
 	initialCards
 } from "./components/initialData.js";
 
-
-
 import {
-	// openPopup,
 	closePopup,
 	openPopupProfile,
 	openPopupAddCard,
-	increaseImage
 } from "./components/modal.js";
 
-// import {
-// 	handleProfileFormSubmit,
-// 	handleCardFormSubmit,
-// 	createCard,
-// 	addLike,
-// 	handleDelButton
-// } from "./components/card";
+import {
+	elements,
+	createCard
+} from "./components/card.js";
 
 
 
@@ -45,16 +38,6 @@ const profileName = document.querySelector('.profile__name');
 const profileInformation = document.querySelector('.profile__information');
 
 
-// Увеличение изображения
-const popupImage = document.querySelector('.popup_type_image');
-const imageZoom = document.querySelector('.popup__image');
-const imageTitle = document.querySelector('.popup__image-title');
-
-const elementTemplate = document.querySelector('#element').content;
-const elements = document.querySelector('.elements');
-// const element = document.querySelector('.element');
-
-
 // Функция добавление лайка
 export function addLike(like) {
 	like.addEventListener('click', (e) => {
@@ -70,38 +53,13 @@ export function handleDelButton(button, element) {
 	});
 }
 
-// Создание карточки
-export function createCard(elementName, elementLink) {
-	const cardElement = elementTemplate.querySelector('.element').cloneNode(true);
-	const elementImage = cardElement.querySelector('.element__image');
-	elementImage.src = elementLink;
-	elementImage.alt = elementName;
-
-	const elementHeader = cardElement.querySelector('.element__header');
-	elementHeader.textContent = elementName;
-
-	// Добавление лайка
-	const like = cardElement.querySelector('.element__like');
-	addLike(like);
-
-	// Удаление карочки
-	const deleteButton = cardElement.querySelector('.element__delete');
-	handleDelButton(deleteButton, cardElement);
-
-	// Увеличение изображения
-	elementImage.addEventListener('click', (e) => {
-		increaseImage(popupImage, imageZoom, imageTitle, elementLink, elementName);
-	});
-
-	return cardElement;
-}
-
-
-// Добавление карточки на страницу
-export function addСardToPage(elementName, elementLink) {
+function addСardToPage(elementName, elementLink) {
 	const card = createCard(elementName, elementLink);
 	elements.prepend(card);
 }
+
+
+
 
 // Добавление карточки из формы
 export function handleCardFormSubmit(e) {
